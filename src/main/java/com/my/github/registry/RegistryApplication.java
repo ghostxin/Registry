e55212565a652b28fb486653b2d8aba3acea798a
赵鑫 <908750258@qq.com>
2022-04-19 10:51:33 +0800

    初始化

--- src/main/java/com/my/github/registry/RegistryApplication.java
+++ src/main/java/com/my/github/registry/RegistryApplication.java
@@ -1,4 +1,4 @@
-package com.sogal.registry;
+package com.my.github.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
